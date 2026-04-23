5798091f080231d937b97a5d1b9a14a41ba68b4b
Suraj Sharma <your-suraj.itees.1256@gmail.com>
2026-04-23 15:47:02 +0530

    fix vercel dist issue

--- src/Component/Shoes/Puma.jsx
+++ src/Component/Shoes/Puma.jsx
@@ -12,7 +12,7 @@ function Puma() {
   const { addToCart } = useCart();
   const getProducts = async () => {
     try {
-      const res = await axios.get("https://stich-rose.vercel.app/api/products");
+      const res = await axios.get("http://localhost:5000/api/products");
       setProducts(res.data.products || res.data);
     } catch (error) {
       console.log("Error fetching products:", error);
